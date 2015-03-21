a12728af2709999fd11bebdd9aaa42de1da68aac
Rene Stuifzand <renestuifzand@gmail.com>
2015-03-21 15:43:51 +0100

    This is for Coursera. My Programming Assignment 2: Lexical Scoping.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Function that gives back cached data or stores it in the global environment
+makeCacheMatrix <- function(x = numeric()) {
 
-makeCacheMatrix <- function(x = matrix()) {
-
+  ## empty inverse variable
+  i <- NULL
+  
+  ## set matrix to cache the data
+    set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  ## get the matrix
+  get <- function() {
+    x
+  }
+  
+  ## set the inverse variable
+  setinverse <- function(inverse) i <<- inverse
+  
+  ## get the inverse variable
+  getinverse <- function() i
+    
+  # creates a list to store the four functions
+  list(set = set,
+       get = get, 
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Function that calls the stored inverse, or creates it and than sets the inverse 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## gets the inverse
+  i <- x$getinverse()
+  
+  ## If there is a inverse found than you return a message and the inverse value
+  if(!is.null(i)) {
+    
+    message("getting cached data")
+    return(i)
+    
+  }
+  
+  ## if inverse is not found than get the data matrix
+  data <- x$get()
+  
+  ## creates the inverse of the matrix with the generic function solve
+  i <- solve(data, ...)
+  
+  ## set the inverse
+  x$setinverse(i) 
+  
+  i  
+
 }
